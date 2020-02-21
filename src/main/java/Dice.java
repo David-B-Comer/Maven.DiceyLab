19827abd36c3936facdcc0c445a15ffad8dd7776
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-02-21 14:33:18 -0500

    working through it

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,29 @@
+import com.sun.tools.javac.util.Position;
+
+import java.util.Random;
+
 public class Dice {
 
+private int dice;
+private int numberOfDice;
+private Random rgen;
+private int numberOfTosses;
 
+public Dice(int numberOfTosses) {
+    this.numberOfTosses = numberOfTosses;
+    rgen = new Random();
+}
+
+public Integer toss() {
+    int counterForTosses = 0;
+    for (int i = 0; i < rgen.nextInt(); i++) {
+        counterForTosses += (rgen.nextInt(6)+1);
+    }
+    return counterForTosses;
+}
+
+public Dice(int numberOfTosses, long seed) {
+    this.numberOfTosses = numberOfTosses;
+    rgen = new Random(seed);
+}
 }
---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.Assert;
+
+public class DiceTest {
+
+    @Test
+    void toss() {
+        // given
+        //roll dice
+        //Dice dice = new Dice(3, 7);
+
+        // when
+
+        //Integer expected = dice.toss();
+
+        // then
+        //returns a number from 1-6
+
+        //Assert.assertEquals(expected, actual);
+
+
+    }
+
+
+}
